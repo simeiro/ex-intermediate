4947fc3a3e12ecf9d6b378dd64deedc06e46b189
simeiro <shitimi1633@gmail.com>
2026-05-26 15:42:32 +0900

    ホテルドメインを作成

--- src/main/java/com/example/ex_intermediate/domain/Hotel.java
+++ src/main/java/com/example/ex_intermediate/domain/Hotel.java
@@ -1,7 +1,30 @@
 package com.example.ex_intermediate.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
- *
+ * ホテルのドメイン.
  */
+@Getter
+@Setter
+@ToString
 public class Hotel {
+    /**
+     * id.
+     */
+    private Long id;
+    /**
+     * ホテル名.
+     */
+    private String hotelName;
+    /**
+     * 最寄駅.
+     */
+    private String nearestStation;
+    /**
+     * 金額.
+     */
+    private Integer price;
 }
