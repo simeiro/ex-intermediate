5fe76773ecbae7ece1cd2b911fc844b4a031161e
simeiro <shitimi1633@gmail.com>
2026-05-26 12:20:11 +0900

    野球コントローラーの型を作成

--- src/main/java/com/example/ex_intermediate/controller/BaseballController.java
+++ src/main/java/com/example/ex_intermediate/controller/BaseballController.java
@@ -1,7 +1,31 @@
 package com.example.ex_intermediate.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 野球のコントローラー.
+ */
 @Controller
+@RequestMapping("/baseball")
 public class BaseballController {
+
+    /**
+     *
+     * @return
+     */
+    @GetMapping("")
+    public String index() {
+        return "team-list";
+    }
+
+    /**
+     *
+     * @return
+     */
+    @GetMapping("/detail")
+    public String detail() {
+        return "detail";
+    }
 }
