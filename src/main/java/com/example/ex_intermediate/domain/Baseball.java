4a104f6c8a817f8a979e296c6e4c11d28221b933
simeiro <shitimi1633@gmail.com>
2026-05-26 11:55:39 +0900

    野球ドメインを作成

--- src/main/java/com/example/ex_intermediate/domain/Baseball.java
+++ src/main/java/com/example/ex_intermediate/domain/Baseball.java
@@ -1,4 +1,31 @@
 package com.example.ex_intermediate.domain;
 
+/**
+ * 野球のドメイン.
+ */
 public class Baseball {
+    /**
+     * id.
+     */
+    private Long id;
+    /**
+     * リーグの名前.
+     */
+    private String leagueName;
+    /**
+     * チームの名前.
+     */
+    private String teamName;
+    /**
+     * 本拠地.
+     */
+    private String headquarters;
+    /**
+     * 発足.
+     */
+    private String inauguration;
+    /**
+     * 歴史.
+     */
+    private String history;
 }
