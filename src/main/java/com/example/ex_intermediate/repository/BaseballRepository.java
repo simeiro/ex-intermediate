96833080262e3f2de7a1317eb78cfcef4e8db275
simeiro <shitimi1633@gmail.com>
2026-05-26 12:00:06 +0900

    必要なbaseballリポジトリの型を作成

--- src/main/java/com/example/ex_intermediate/repository/BaseballRepository.java
+++ src/main/java/com/example/ex_intermediate/repository/BaseballRepository.java
@@ -1,4 +1,27 @@
 package com.example.ex_intermediate.repository;
 
+import com.example.ex_intermediate.domain.Baseball;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class BaseballRepository {
+
+    @Autowired
+    NamedParameterJdbcTemplate template;
+
+    private static final RowMapper<Baseball> BASEBALL_ROW_MAPPER = new BeanPropertyRowMapper<>(Baseball.class);
+
+    public List<Baseball> findAllTeamName() {
+        return null;
+    }
+
+    public Baseball findById(Long id) {
+        return null;
+    }
 }
