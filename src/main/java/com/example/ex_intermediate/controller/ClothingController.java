aebbbe3d799a10cbe801a101485ea1ba1fbe3d6b
simeiro <shitimi1633@gmail.com>
2026-05-27 11:13:38 +0900

    衣類情報を操作するコントローラーの型を作成

--- src/main/java/com/example/ex_intermediate/controller/ClothingController.java
+++ src/main/java/com/example/ex_intermediate/controller/ClothingController.java
@@ -1,12 +1,32 @@
 package com.example.ex_intermediate.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- *
+ * 衣類情報を操作するコントローラー.
  */
 @Controller
 @RequestMapping("/clothing")
 public class ClothingController {
+
+    /**
+     *
+     * @return
+     */
+    @GetMapping("")
+    public String index() {
+        return "clothing/search";
+    }
+
+    /**
+     *
+     * @return
+     */
+    @PostMapping("/search")
+    public String search() {
+        return "clothing/search";
+    }
 }
