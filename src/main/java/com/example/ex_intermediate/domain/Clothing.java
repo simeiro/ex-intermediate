f3d452d46700b748c9893a5502886e54eadadb0a
simeiro <shitimi1633@gmail.com>
2026-05-27 10:09:56 +0900

    衣類のドメインを作成

--- src/main/java/com/example/ex_intermediate/domain/Clothing.java
+++ src/main/java/com/example/ex_intermediate/domain/Clothing.java
@@ -1,7 +1,29 @@
 package com.example.ex_intermediate.domain;
 
 /**
- *
+ * 衣類のドメイン.
  */
 public class Clothing {
+    /**
+     * 性別.
+     * 0は男性、1は女性。
+     */
+    private Integer gender;
+    /**
+     * 色.
+     */
+    private String color;
+    /**
+     * ジャンル.
+     */
+    private String genre;
+    /**
+     * サイズ.
+     * S, M, L の三段階。
+     */
+    private String size;
+    /**
+     * 価格.
+     */
+    private Integer price;
 }
