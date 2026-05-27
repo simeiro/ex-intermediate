fac4fc7ff6322e08894fa3dc1a02d5600c9d02d8
simeiro <shitimi1633@gmail.com>
2026-05-27 11:06:24 +0900

    衣類情報を操作するサービスを作成

--- src/main/java/com/example/ex_intermediate/service/ClothingService.java
+++ src/main/java/com/example/ex_intermediate/service/ClothingService.java
@@ -1,12 +1,31 @@
 package com.example.ex_intermediate.service;
 
+import com.example.ex_intermediate.domain.Clothing;
+import com.example.ex_intermediate.repository.ClothingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 /**
- *
+ * 衣類情報を操作するサービス.
  */
 @Service
 @Transactional
 public class ClothingService {
+
+    @Autowired
+    private ClothingRepository repository;
+
+    /**
+     * 指定した性別、色に一致する衣類情報一覧を取得する.
+     *
+     * @param gender 性別(0:男性, 1:女性)
+     * @param color  色
+     * @return　指定した性別、色に一致する衣類情報一覧
+     */
+    public List<Clothing> searchByGenderAndColor(Integer gender, String color) {
+        return repository.findByGenderAndColor(gender, color);
+    }
 }
